d31b32ca8f8c67bbb6ccc335d69c0dffd881281d
Hazel Azucena Calderón Bonilla <cb22014@ues.edu.sv>
2025-05-19 12:33:26 -0600

    validar archivo usuarios.xml, update main activity

--- app/src/main/java/sv/edu/ues/dam135/evaluacionpractica1/MainActivity.java
+++ app/src/main/java/sv/edu/ues/dam135/evaluacionpractica1/MainActivity.java
@@ -1,11 +1,15 @@
 package sv.edu.ues.dam135.evaluacionpractica1;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 import androidx.activity.EdgeToEdge;
@@ -42,6 +46,28 @@ public class MainActivity extends AppCompatActivity {
                     .setNegativeButton("No", null)
                     .show();
         });
+
+        EditText edtUsuario = findViewById(R.id.edtUsuario); // ingreso con email, no con nombre
+        EditText edtPassword = findViewById(R.id.edtPassword);
+
+        Button btnIngresar = findViewById(R.id.btnIngresar);
+
+        btnIngresar.setOnClickListener(v -> {
+            String emailIngresado = edtUsuario.getText().toString().trim();
+            String passIngresado = edtPassword.getText().toString().trim();
+
+            SharedPreferences prefs = getSharedPreferences("usuarios", MODE_PRIVATE);
+            String emailGuardado = prefs.getString("email", "");
+            String passGuardado = prefs.getString("password", "");
+
+            if (emailIngresado.equals(emailGuardado) && passIngresado.equals(passGuardado)) {
+                Toast.makeText(this, "¡Bienvenido! Si se pudo", Toast.LENGTH_SHORT).show();
+                //Para mientras
+            } else {
+                Toast.makeText(this, "Correo o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
